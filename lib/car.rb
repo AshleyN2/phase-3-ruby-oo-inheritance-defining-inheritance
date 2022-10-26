70cca720c7e84e2ed5b716f89578cc9342da39b6
Ashley <ashleynjeri7@gmail.com>
2022-10-26 20:36:07 +0300

    code along

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,11 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+
+# Defining Subclasses
+class Car < Vehicle
+    # Overwrites the inherited class method from Vehicle
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+      end
+
+end
